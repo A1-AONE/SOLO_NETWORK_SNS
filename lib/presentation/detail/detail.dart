baa91e35ee198ae5a44ccd5cd53c463a27eda010
chabugon <qnrhs0913@gmail.com>
2025-01-06 10:31:35 +0900

    [feat]feed, detail UI 기초

---
+++ lib/presentation/detail/detail.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:solo_network_sns/presentation/detail/widgets/detail_comment.dart';
+import 'package:solo_network_sns/presentation/detail/widgets/detail_info.dart';
+
+class Detail extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          DetailInfo(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+            child: Text('2025년 01월 03일'),
+          ),
+          Divider(thickness: 1, color: Colors.black),
+          DetailComment(),
+          DetailComment(),
+          DetailComment(),
+          DetailComment(),
+          DetailComment(),
+        ],
+        
+      ),
+    );
+  }
+}
