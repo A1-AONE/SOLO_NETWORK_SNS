57aaa64e66febe5cf2abd6f63422c4b333163b5a
UooUzzz <uuumonazzz@gmail.com>
2025-01-06 20:55:55 +0900

    feat: splash

--- lib/presentation/app/router.dart
+++ lib/presentation/app/router.dart
@@ -1,21 +1,25 @@
 import 'package:go_router/go_router.dart';
 import 'package:solo_network_sns/presentation/ui/create_page/create_page.dart';
 import 'package:solo_network_sns/presentation/ui/detail/detail.dart';
-import 'package:solo_network_sns/presentation/ui/feed/feed_page.dart';
 import 'package:solo_network_sns/presentation/ui/home_tap/home_tap.dart';
 import 'package:solo_network_sns/presentation/ui/login/login_page.dart';
 import 'package:solo_network_sns/presentation/ui/setpage/setpage.dart';
+import 'package:solo_network_sns/presentation/ui/splash/splash_screen.dart';
 
 final router = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/splash',
   routes: [
+    GoRoute(
+      path: '/splash',
+      builder: (context, state) => SplashScreen(),
+    ),
     GoRoute(
       path: '/login',
       builder: (context, state) => LoginPage(),
       routes: [
         GoRoute(
           path: 'set',
-          builder: (context, state) => Setpage(),
+          builder: (context, state) => SetPage(),
         ),
       ],
     ),
