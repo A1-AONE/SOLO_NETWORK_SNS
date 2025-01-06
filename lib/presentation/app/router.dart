788d347ab896fb5d12be62f7f29caeb1232b8800
seonghoon-02 <tutle02@gmail.com>
2025-01-06 16:05:46 +0900

    [fix]: pull 후 충돌 해결

--- lib/presentation/app/router.dart
+++ lib/presentation/app/router.dart
@@ -1,31 +1,20 @@
 import 'package:go_router/go_router.dart';
-import 'package:solo_network_sns/presentation/detail/detail.dart';
-import 'package:solo_network_sns/presentation/feed/feed_page.dart';
+import 'package:solo_network_sns/presentation/ui/detail/detail.dart';
+import 'package:solo_network_sns/presentation/ui/feed/feed_page.dart';
 import 'package:solo_network_sns/presentation/ui/login/login_page.dart';
 import 'package:solo_network_sns/presentation/ui/setpage/setpage.dart';
 
-final router = GoRouter(
-  initialLocation: '/login/set',
-  routes: [
+final router = GoRouter(initialLocation: '/login/set', routes: [
+  GoRoute(path: '/login', builder: (context, state) => LoginPage(), routes: [
     GoRoute(
-      path: '/login',
-      builder: (context, state) => LoginPage(),
-      routes: [
-        GoRoute(
-          path: 'set',
-          builder: (context, state) => Setpage(),
-        )
-      ]
-    ),
-    GoRoute(
-      path: '/',
-      builder: (context, state) => FeedPage(),
-      routes: [
-        GoRoute(
-          path: 'feed',
-          builder: (context, state) => Detail(),
-        )
-      ]
+      path: 'set',
+      builder: (context, state) => Setpage(),
     )
-  ]
-);
+  ]),
+  GoRoute(path: '/', builder: (context, state) => FeedPage(), routes: [
+    GoRoute(
+      path: 'feed',
+      builder: (context, state) => Detail(),
+    )
+  ])
+]);
