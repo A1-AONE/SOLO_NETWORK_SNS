0638a5ee1cf5acc1d81401be7fcffbf76d3fe7da
seonghoon-02 <tutle02@gmail.com>
2025-01-06 16:56:38 +0900

    [feat]: db 수정중

--- lib/presentation/app/router.dart
+++ lib/presentation/app/router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:solo_network_sns/presentation/ui/create_page/create_page.dart';
 import 'package:solo_network_sns/presentation/ui/detail/detail.dart';
 import 'package:solo_network_sns/presentation/ui/feed/feed_page.dart';
 import 'package:solo_network_sns/presentation/ui/login/login_page.dart';
@@ -8,7 +9,7 @@ final router = GoRouter(initialLocation: '/login/set', routes: [
   GoRoute(path: '/login', builder: (context, state) => LoginPage(), routes: [
     GoRoute(
       path: 'set',
-      builder: (context, state) => Setpage(),
+      builder: (context, state) => CreatePage(), //Setpage()
     )
   ]),
   GoRoute(path: '/', builder: (context, state) => FeedPage(), routes: [
