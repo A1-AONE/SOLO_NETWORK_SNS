6d7cbaf0864d02f82aa85e71a759222bbf468c27
UooUzzz <uuumonazzz@gmail.com>
2025-01-08 19:09:32 +0900

    add: login repository impl

--- lib/data/source/login_data_source_impl.dart
+++ lib/data/source/login_data_source_impl.dart
@@ -1,21 +1,73 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-// import 'package:solo_network_sns/data/source/login_data_source.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:solo_network_sns/data/source/login_data_source.dart';
 
-// // class LoginDataSourceImpl implements LoginDataSource {
-// //   final GoogleSignIn _googleSignIn;
-// //   final FirebaseAuth _auth;
-// //   final FirebaseFirestore _firestore;
+// LoginDataSource에서 정의된 메서드 실제로 impl 하는 부분
+// google sign in 이랑 firebase 연동
+// firestore에 사용자 정보 저장
 
-// //   LoginDataSourceImpl({
-// //     required GoogleSignIn googleSignIn,
-// //     required FirebaseAuth auth,
-// //     required FirebaseFirestore firestore,
-// //   }) : _googleSignIn = googleSignIn,
-// //        _auth = auth,
-// //        _firestore = firestore;
+class LoginDataSourceImpl implements LoginDataSource {
+  final GoogleSignIn _googleSignIn;
+  final FirebaseAuth _auth;
+  final FirebaseFirestore _firestore;
 
-// //   @override
-// //   Future<GoogleSignInAuthentication?>
-// // }
+  LoginDataSourceImpl({
+    required GoogleSignIn googleSignIn,
+    required FirebaseAuth auth,
+    required FirebaseFirestore firestore,
+  })  : _googleSignIn = googleSignIn,
+        _auth = auth,
+        _firestore = firestore;
+
+  @override
+  Future<GoogleSignInAccount?> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await _googleSignIn.signIn();
+      return googleSignInAccount;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<UserCredential> signInWithFirebase(OAuthCredential credential) async {
+    try {
+      final userCredential = await _auth.signInWithCredential(credential);
+      return userCredential;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<QuerySnapshot<Object?>> fetchUserEmail(String email) async {
+    try {
+      final snapshot = await _firestore
+          .collection('User')
+          .where('email', isEqualTo: email)
+          .get();
+      return snapshot;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> saveNewUser(String uid, String email) async {
+    try {
+      final userDoc = _firestore.collection('User').doc(uid);
+      await userDoc.set({
+        'AITag': [],
+        'Nickname': '',
+        'isCanSpying': false,
+        'profileUrl': '',
+        'uid': uid,
+        'email': email,
+      });
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
