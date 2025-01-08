472265c1cc65958e4146611ed625cc68de25eaab
UooUzzz <uuumonazzz@gmail.com>
2025-01-08 17:24:21 +0900

    add: login datasource

---
+++ lib/data/source/login_data_source_impl.dart
@@ -0,0 +1,21 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:solo_network_sns/data/source/login_data_source.dart';
+
+// // class LoginDataSourceImpl implements LoginDataSource {
+// //   final GoogleSignIn _googleSignIn;
+// //   final FirebaseAuth _auth;
+// //   final FirebaseFirestore _firestore;
+
+// //   LoginDataSourceImpl({
+// //     required GoogleSignIn googleSignIn,
+// //     required FirebaseAuth auth,
+// //     required FirebaseFirestore firestore,
+// //   }) : _googleSignIn = googleSignIn,
+// //        _auth = auth,
+// //        _firestore = firestore;
+
+// //   @override
+// //   Future<GoogleSignInAuthentication?>
+// // }
