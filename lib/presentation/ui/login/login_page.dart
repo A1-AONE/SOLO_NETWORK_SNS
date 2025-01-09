a055a391b8002f1c802ff3457376a85854a91b4d
UooUzzz <uuumonazzz@gmail.com>
2025-01-09 11:08:28 +0900

    fix: ooo

--- lib/presentation/ui/login/login_page.dart
+++ lib/presentation/ui/login/login_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:solo_network_sns/presentation/ui/login/login_view_model.dart';
-import 'package:solo_network_sns/presentation/viewmodel/user_id.dart';
 
 class LoginPage extends StatelessWidget {
   // void loginInWithGoogle(BuildContext context, WidgetRef ref) async {
